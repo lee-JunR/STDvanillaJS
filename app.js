f5fdbbbe1a38bf975df6681bfa11e1e6d24ba427
lee-JunR <wns2610@icloud.com>
2023-10-03 15:56:29 +0900

    prac

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
-/** 클릭시 h1을 다른 색으로 바꾸는 함수*/
-function handleTitleClick() {
-    const activeClass = "active"
-    if(h1.classList.contains(activeClass)){
-        h1.classList.remove(activeClass);
-    }else{
-        h1.classList.add(activeClass);
-    }
-}
-// 두 함수는 같은 기능.
-function handleTitleClick() {
-    h1.classList.toggle("active")
+function onLoginSubmit(event) {
+   event.preventDefault();
+   console.log(loginInput.value);
 }
 
-
-h1.addEventListener("click",handleTitleClick);
+loginForm.addEventListener("submit", onLoginSubmit);
