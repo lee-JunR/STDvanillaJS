72da0229731a250e8ba6c5745551151105a4ccdb
lee-JunR <wns2610@icloud.com>
2023-10-03 17:10:23 +0900

    목적 : 유저가 이름을 제출하면 1. 해당 loginForm 이 보이지 않게 하고 2. 제출된 유저 이름이 담긴 h1을 보여주고 싶음

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
+
+// 목적 : 유저가 이름을 제출하면 1. 해당 loginForm 이 보이지 않게 하고 2. 제출된 유저 이름이 담긴 h1을 보여주고 싶음 
+
+
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting"); //css selector를 사용하여 greeting 이라는 id를 가지고 있는 요소를 가져옴
 
-/** submit Event 확인을 위한 함수 */
-function onLoginSubmit(event) {  //버튼 눌렀을 때 일어나는 submit 이벤트에 대한 정보를 event 인자에 넣어줌.
-   event.preventDefault();       //submit 이벤트가 일어나면 새로고침 되므로 이를 방지함. (preventDefault(): 기본 동작 방지)
-   console.log(event);           //이벤트에 대한 정보를 콘솔로그에 띄움
-   console.log(loginInput.value);//새로고침이 일어나지 않아 loginInput value 를 콘솔에서 볼 수 있음
+const HIDDEN_CLASSNAME = "hidden"; //css의 hidden class.
+
+/** 유저가 이름을 입력하면 1. 해당 Form 이 사라지게 하고 2. h1을 나타나게 하는 함수 */
+function onLoginSubmit(event) {                 //버튼 눌렀을 때 일어나는 submit 이벤트에 대한 정보를 event 인자에 넣어줌.
+                                             //1.
+   event.preventDefault();                      //submit 이벤트가 일어나면 새로고침 되므로 이를 방지함. (preventDefault(): 기본 동작 방지)
+   const username = loginInput.value;           //username 변수에 loginInput.value 를 부여.
+   loginForm.classList.add(HIDDEN_CLASSNAME);   //css의 hidden class를 loginForm에 부여함.
+                                             //2.
+   console.log(username);                       
+   greeting.innerText = "hello " + username;     //<h1>이 담긴 변수  greeting 에 username 추가
+   greeting.innerText = `Hello ${username}`     //<h1>이 담긴 변수  greeting 에 username 추가
+
+   greeting.classList.remove(HIDDEN_CLASSNAME);  //greeting에 class 가 hidden 이라 안보이므로 hidden 클래스 삭제
 }
 
-/** pointer Event 확인을 위한 함수 */
-function onLinkClick(event){  //링크를 누르면 pointer 이벤트에 대한 정보를 event 인자에 넣어줌
-   event.preventDefault();
-   console.log(event);        //pointer event 에는 이벤트 시각, x 좌표 y 좌표 등의 정보가 들어가 있음.
-   alert("clicked!");
-}
 loginForm.addEventListener("submit", onLoginSubmit);
-
-link.addEventListener("click", onLinkClick);
